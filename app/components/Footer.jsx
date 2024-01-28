b70d204ab6f261594ae55eed92fce20aa0be90cd
Hassan Agmir <hagmir6@gmail.com>
2024-01-28 23:03:13 +0100

    Book detail and category

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
             </div>
             <p className='menu d-flex justify-content-center mt-3 flex-wrap'>
               <Link href='/' className='nav-link mx-2'>Home</Link>
-              <Link href='/pages/privacy' className='nav-link mx-2'> Privacy Policy</Link>
+              <Link className='nav-link mx-2' href={'/pages/privacy'}> Privacy Policy</Link>
               <Link href='/pages/about' className='nav-link mx-2'>About</Link>
               <Link href='/pages/contact' className='nav-link mx-2'>Contact</Link>
             </p>
