90bb663147114d1bcdb0710a9b34ab6cbf6b99a5
Hassan Agmir <hagmir6@gmail.com>
2024-01-29 12:36:05 +0100

    Update fetlify.toml file

--- app/components/Books.jsx
+++ app/components/Books.jsx
@@ -7,7 +7,8 @@ export default async function Books(props) {
   const response = await fetch(`https://freesad.com/api/books/${props.category ? props.category : ''}`)
   const books = await response.json()
   return (
-    <div className='row pb-5'>
+    <div className='row pb-5 p-0 m-0'>
+      <h1 className='d-none'>Download free PDF books</h1>
       {books.data.map((book) => (
         <BookCard
           key={book.slug}
