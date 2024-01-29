4c7cf621b4ad2b0eb007d4b002bd65b9dcf4eaea
Hassan Agmir <hagmir6@gmail.com>
2024-01-29 13:04:20 +0100

    Update fetlify.toml file

--- app/book/[slug]/page.jsx
+++ app/book/[slug]/page.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import Books from '@/app/components/Books'
+import DownloadBook from '@/app/components/DownloadBook'
 
-export default function BookPage() {
-  return (
-    <div>Book Page</div>
-  )
+export default function Page() {
+  const router = useRouter()
+  const { slug } = router.query
+
+  const fetchBook = async () => {
+    const response = await fetch(`https://freesad.com/api/book/${slug}`)
+    const book = await response.json()
+    return book
+  }
+
+  const language = 'en'
+  const book = fetchBook()
+
+  if (!book) {
+    return <div>Loading...</div>
+  }
+  return <div>{book.name}</div>
 }
--- app/book/page.jsx
+++ app/book/page.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import Link from 'next/link'
-import Books from '@/app/components/Books'
-import DownloadBook from '@/app/components/DownloadBook'
+import React from 'react'
 
-export default async function page() {
-  const response = await fetch('https://freesad.com/api/book/' + props.params.slug)
-  const book = await response.json()
-  const options = {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit',
-    timeZone: 'UTC',
-  }
-
-  const language = "en" // navigator.language.slice(0, 2)
+export default function page() {
   return (
-    <div>{book.name}</div>
+    <div>page</div>
   )
 }
