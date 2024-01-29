4ecada7050c327ef673b96a22f49e7e065b387bf
Hassan Agmir <hagmir6@gmail.com>
2024-01-29 12:53:06 +0100

    Update fetlify.toml file

--- app/book/page.jsx
+++ app/book/page.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import React from 'react'
 import Link from 'next/link'
 import Books from '@/app/components/Books'
 import DownloadBook from '@/app/components/DownloadBook'
 
-export default function page() {
+export default async function page() {
   return (
     <div>page</div>
   )
