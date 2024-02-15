9fa379f6156b38b393f399be4523d5e6469ad1c7
Hassan Agmir <hagmir6@gmail.com>
2024-02-15 13:04:51 +0100

    update sitemap.xml

--- app/p/[slug]/page.jsx
+++ app/p/[slug]/page.jsx
@@ -15,7 +15,7 @@ export async function generateMetadata({ params, searchParams }, parent) {
     image: 'https://freesad.com' + post.image,
     keywords: ['posts', post.tags],
     alternates: {
-      canonical: '/p/' + post.slug,
+      canonical: 'https://wwww.freewsad.com/p/' + post.slug,
     },
     openGraph: {
       title: post.title,
