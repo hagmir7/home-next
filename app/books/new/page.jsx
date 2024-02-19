ce881398d1c01bc21a4c31ebccc4460833a63893
Hassan Agmir <hagmir6@gmail.com>
2024-02-19 19:57:40 +0100

    fex errors and ready to test in production

--- app/books/new/page.jsx
+++ app/books/new/page.jsx
@@ -1,29 +1,29 @@
-import Books from '../components/Books'
-import SearchBook from '../components/SearchBook'
+import Books from '@/app/components/Books'
+import SearchBook from '@/app/components/SearchBook'
 
 export const metadata = {
-  title: 'Download New & Free PDF books',
+  title: 'Download New free PDF books',
 
   alternates: {
-    canonical: 'https://www.freewsad.com/book',
+    canonical: 'https://www.freewsad.com/books/new',
   },
   openGraph: {
-    title: 'Download New & Free PDF books',
+    title: 'Download New free PDF books',
     images: '/thumbnail.png',
     description:
       'You can enjoy the Topics and Books you love and download the original content, and share it all with your friends in FreeWsad.',
-    url: '/book',
+    url: '/books/new',
     type: 'website',
     image: {
       url: '/thumbnail.png',
-      alt: 'Download New & Free PDF books',
+      alt: 'Download New free PDF books',
       width: 600,
       height: 800,
     },
   },
 }
 
-export default function Home() {
+export default function Trending() {
   return (
     <main className='container'>
       <div className='row my-3 d-flex justify-content-center'>
@@ -36,8 +36,8 @@ export default function Home() {
       </div>
       <div className='row'>
         <Books
-          url={`https://freesad.com/en/api/books/new`}
-          title={<h1 className='d-none'>Download New & free PDF books</h1>}
+          url={`https://freesad.com/en/api/books`}
+          title={<h1 className='d-none'>Download New free PDF books</h1>}
         />
       </div>
     </main>
