69be12f6e511f739445f29c4ce1a89458ee34ee2
Hassan Agmir <hagmir6@gmail.com>
2024-05-23 20:49:52 +0100

    Update google search consol

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -64,6 +64,7 @@ export default async function RootLayout({ children, params :{locale} }) {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
+          <meta name="google-site-verification" content="S80_9wpz81cproUDSD8eSH6043BoZwUoaNYmATG1Pj4" />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6043226569102012" crossOrigin="anonymous" ></script>
         </head>
         <AuthProvider>
